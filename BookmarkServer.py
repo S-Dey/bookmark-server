4c7db13d97ecf1eb6aa606ced345d444bb765dae
Subhadeep Dey <contact.sdey@gmail.com>
2018-04-24 12:06:27 +0530

    Fix bug

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -110,5 +110,5 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
     server_address = ('', port)
-    httpd = HTTPServer(server_address, Shortener)
+    httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
